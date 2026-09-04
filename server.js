95faf5d8d908068d6da065bfce8fdb0e08aa4e4b
admin <admin@admins-MBP.lan>
2026-09-04 07:20:56 +0100

    ejs added

--- server.js
+++ server.js
@@ -3,12 +3,25 @@ import session from "express-session"
 import authRouter from "./routes/auth.js"
 import { meRouter } from "./routes/me.js"
 import 'dotenv/config';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express()
 
+//use ejs templates
+app.set('view engine', 'ejs')
+
+//serve any file inside the 'public' folder publicly
+app.use(express.static(path.join(__dirname, 'public')));
+
 const secret = process.env.SECRET
-//to be able to grab body from api form
+//to be able to grab body from api ajax
 app.use(express.json())
+//to be able to grab body from form
+app.use(express.urlencoded({ extended: true }));
 
 app.use(session({
     secret: secret,
